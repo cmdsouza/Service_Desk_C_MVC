dfd2acf80e91571a5e117ea26235c982ecbd9f86
Carlos Matheus <cmsouza1998@gmail.com>
2022-06-12 15:58:19 -0300

    Criação da página do layout mestre

---
+++ service_desk/service_desk/Controllers/LayoutMestreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace service_desk.Controllers
+{
+    public class LayoutMestreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
